5f8bbcec67298ce97d03d1eb5cc890c86644ccb8
MarcoFedez1 <49213256+MarcoFedez1@users.noreply.github.com>
2019-04-12 11:22:14 -0600

    Update search_freelancers.rb

--- UpWorkTest/search_freelancers.rb
+++ UpWorkTest/search_freelancers.rb
@@ -4,18 +4,25 @@ require_relative 'elements.rb'
 keyword = 'Marco'
 
   
-      #define new browser
+  #define new browser
+  puts "1- Navigare to http://upwork.com"
   browser=SiteElement.new("http://upwork.com")
   
-      #Send keys to freelancers bar
+  #Send keys to freelancers bar
+  puts "2- Click on Free Lancers Bsearch bar"
+  puts "3- Enter the keywork for the search"
+  print "Enter the freelancer name: "
+  keyword = gets
   browser.freelancers_search_bar().send_keys(keyword)
   
-      #click en search
+  #click en search
+  print "/n"
+  puts "4- Click on search button"
   browser.search_bnt().click
   
   browser.wait_for(2)
   
-      #verify if the search have results displayed
+  #verify if the search have results displayed
   if browser.results().size > 0
     results = browser.results()
       i = 0
@@ -27,23 +34,21 @@ keyword = 'Marco'
   else 
     puts "No results found"
   end
+  puts "5- Select any freelancers"
   num = rand(3)
   results[num].click
   if (browser.freelancers_name() )
     freelancer_name = browser.freelancers_name()
     def includes
       if freelancer_name.include?(freelancers[num])
+  
         return freelancer_name.include?(freelancers[num])
       else  
         puts "The freelancers name are not the same"
       end
     end
-    puts freelancer_name + ' - ' + freelancers[num]
   else
     "The freelancers profile are not available"
   end
-
-
-
-
+  puts "The freelancer selected was " +  freelancers[num]
 browser.close_browser
